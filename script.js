cbf3135258e51200315e93f8e7d8b1e871954977
Benson Njuguna <njugunab655@gmail.com>
2022-09-17 09:23:32 +0300

    Create an operate function

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function mutiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    if(operator === 'add') {
+        add(a, b)
+    } else if (operator === 'subtract') {
+        subtract(a, b)
+    } else if (operator === 'multiply') {
+        mutiply(a, b) 
+    } else if (operator === 'divide') {
+        divide(a, b)
+    }
+};
